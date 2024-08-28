8d52692b3739eb964e7707025b1215bb09076c7c
Pavel Borisov <pavel.borisov@zetta.systems>
2024-08-28 21:52:54 +0300

    Create sample hardhat project and deploy contract job in the pipeline

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -x
+
+# Go to the hardhat directory and install the dependencies
+echo "Installing dependencies..."
+cd /hardhat
+npm install
+
+# Compile, test and deploy the contract
+echo "Compiling, testing and deploying the contract..."
+npx hardhat compile
+npx hardhat test
+yes | npx hardhat ignition deploy ./ignition/modules/Lock.ts --network local
+
+# Keep the container running
+tail -f /dev/null
